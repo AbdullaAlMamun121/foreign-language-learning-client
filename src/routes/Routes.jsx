7a866c66f8394d26e1ed4d6172bca99caecb337a
Abdulla Al Mamun <hmdabdulla121@gmail.com>
2023-06-10 00:18:03 +0600

    try to update route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -10,58 +10,61 @@ import AddClasses from "../pages/Dashboard/Instructor/AddClasses/AddClasses";
 import MyClasses from "../pages/Dashboard/Instructor/MyClasses/MyClasses";
 import DisplayInstructor from "../pages/DisplayInstructor/DisplayInstructor";
 import DisplayClasses from "../pages/DisplayClasses/DisplayClasses";
+import PrivateRoute from "./PrivateRoute";
+import AdminRoute from "./AdminRoute";
+import InstructorRoute from "./InstructorRoute";
 
 const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main></Main>,
-      children: [
-        {
-            path: "/",
-            element:<Home></Home>
-        },
-        {
-          path:"login",
-          element:<Login></Login>
-        },
-        {
-          path:"signup",
-          element:<SignUp></SignUp>
-        },
-        {
-          path:'displayInstructor',
-          element:<DisplayInstructor></DisplayInstructor>
-        },
-        {
-          path:'displayClasses',
-          element:<DisplayClasses></DisplayClasses>
-        }
-      ],
-    },
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      },
+      {
+        path: "login",
+        element: <Login></Login>
+      },
+      {
+        path: "signup",
+        element: <SignUp></SignUp>
+      },
+      {
+        path: 'displayInstructor',
+        element: <DisplayInstructor></DisplayInstructor>
+      },
+      {
+        path: 'displayClasses',
+        element: <DisplayClasses></DisplayClasses>
+      }
+    ],
+  },
 
-    {
-      path:'dashboard',
-      element:<Dashboard></Dashboard>,
-      children:[
-        {
-          path:'manageClasses',
-          element:<ManageClasses></ManageClasses>
-        },
-        {
-          path:'manageUsers',
-          element:<ManageUsers></ManageUsers>
-        },
-        {
-          path:'addClasses',
-          element:<AddClasses></AddClasses>
-        },
-        {
-          path:'myClasses',
-          element:<MyClasses></MyClasses>
-        }
-      ]
-    }
-    
-  ]);
+  {
+    path: 'dashboard',
+    element: <PrivateRoute><Dashboard></Dashboard></PrivateRoute>,
+    children: [
+      {
+        path: 'manageClasses',
+        element: <PrivateRoute><AdminRoute><ManageClasses></ManageClasses></AdminRoute></PrivateRoute>
+      },
+      {
+        path: 'manageUsers',
+        element: <PrivateRoute><AdminRoute><ManageUsers></ManageUsers></AdminRoute></PrivateRoute>
+      },
+      {
+        path: 'addClasses',
+        element: <PrivateRoute><InstructorRoute><AddClasses></AddClasses></InstructorRoute></PrivateRoute>
+      },
+      {
+        path: 'myClasses',
+        element: <PrivateRoute><InstructorRoute><MyClasses></MyClasses></InstructorRoute></PrivateRoute>
+      }
+    ]
+  }
 
-  export default router;
+]);
+
+export default router;
